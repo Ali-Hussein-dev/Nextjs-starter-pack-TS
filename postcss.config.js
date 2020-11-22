1c72e9fbe0188285cf1886d5fc057d47dbf318a5
Ali <spreskill.app@gmail.com>
2020-11-22 19:16:44 +0100

    update configs & add tailwind components

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,22 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    enabled: true,
-    content: [
-      './src/components/**/*.{ts, tsx,js,jsx}',
-      './src/pages/**/*.{ts, tsx,js,jsx}',
-    ],
-    defaultExtractor: (content) =>
-      content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
 module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      },
-    ],
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+          'postcss-flexbugs-fixes',
+          'tailwindcss',
+          'postcss-import',
+          [
+            'postcss-preset-env',
+            {
+              autoprefixer: {
+                flexbox: 'no-2009',
+              },
+              stage: 3,
+              features: {
+                'custom-properties': false,
+              },
+            },
+          ],
+        ]
+      : ['tailwindcss', 'postcss-import'],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
+// const colors = require('colors');
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-    defaultLineHeights: true,
-    standardFontWeights: true,
-  },
-  purge: false,
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  // purge: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueGray: {
+          50: '#F8FAFC',
+          100: '#F1F5F9',
+          200: '#E2E8F0',
+          300: '#CBD5E1',
+          400: '#94A3B8',
+          500: '#64748B',
+          600: '#475569',
+          700: '#334155',
+          800: '#1E293B',
+          900: '#0F172A',
+        },
+      },
+    },
   },
-  variants: {},
+  variants: {
+    extend: {
+      scale: ['active'],
+      backgroundColor: ['responsive', 'hover', 'focus', 'disabled'],
+      opacity: ['responsive', 'hover', 'focus', 'group-hover'],
+      cursor: ['responsive', 'hover', 'disabled'],
+    },
+  },
   plugins: [],
 };
